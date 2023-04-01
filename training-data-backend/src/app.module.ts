9a0f21be3a97798bbcc3fa4bc8c94594ca81ee41
Santiago Salacodigo <santiago.salamanca7@gmail.com>
2023-04-01 11:21:24 -0500

    Create users resource

--- training-data-backend/src/app.module.ts
+++ training-data-backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './userManual/usersManual.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
